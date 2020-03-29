cab4800b8519273428113a1bcd10b775ea6c8957
Julian Häring <julian@julians-mini.speedport_w_921v_1_46_000>
2020-03-29 18:52:51 +0200

    node modules

--- server/app.js
+++ server/app.js
@@ -22,6 +22,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+
+const projectpath = path.join(__dirname, '..');
+console.log(projectpath);
+// Serve the static files from the React app
+app.use(express.static(path.join(projectpath, 'client/build')));
+
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(projectpath + '/client/build/index.html'));
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
